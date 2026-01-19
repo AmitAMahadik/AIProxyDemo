8d71a6cde5c35ca396b04a0d7bdff4197e9b44ea
Amit Mahadik <amit.mahadik@gmail.com>
2026-01-19 10:15:33 -0800

    Initial Commit

---
+++ AIProxy Demo/AIProxy_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIProxy_DemoApp.swift
+//  AIProxy Demo
+//
+//  Created by Mahadik, Amit on 1/19/26.
+//
+
+import SwiftUI
+
+@main
+struct AIProxy_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
